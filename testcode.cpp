73d2f46df573e9f0e5f91ff96c127f86dae5961a
shunya-kasai <shunya_01_kasai_gst@jp.honda>
2025-07-04 10:10:30 +0900

    first commit

---
+++ testcode.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int x;
+    int y;
+    int result;
+
+    x = 12;
+    y = 4;
+    result = x*y;
+    std::cout << "x × y = " << result << std::endl;
+
+    return 0;
+}
